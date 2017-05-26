c45adf51be6c950a10deeee34ec968e697494c5c
janiceambrose <janiceambrose1@gmail.com>
2017-05-25 20:54:53 -0700

    customize profile more changes

---
+++ app/src/main/java/com/professionalbuddies/pb_revised/CustomizeProfile.java
@@ -0,0 +1,22 @@
+package com.professionalbuddies.pb_revised;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+
+/**
+ * Created by janiceambrose on 5/25/17.
+ */
+
+public class CustomizeProfile extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customize_profile);
+
+
+        }
+
+
+}
